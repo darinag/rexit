1f37eb62ab1bf989bafc01e9459acf53780ea553
stratiev97 <34278093+stratiev97@users.noreply.github.com>
2019-12-02 00:06:05 +0000

    Add files via upload
    SOM for 3 regions - middle east, sub saharan african and south asia

---
+++ SOM_final.R
@@ -0,0 +1,241 @@
+# ************************************************
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial 4.0 International License.
+# ************************************************
+#  PRATICAL BUSINESS ANALYTICS 2019
+#  COM3018 
+#
+# Group: REXIT
+# The Surrey Business School
+# University of Surrey
+# GUILDFORD
+# Surrey GU2 7XH
+#
+# 03 DECEMBER 2019
+#
+# ************************************************
+# R Script For Coursework Assignment
+
+#  clears all objects in "global environment"
+rm(list=ls())
+
+# ************************************************
+# Global Environment variables
+
+DATASET_FILENAME = "globalterrorismdb_0718dist.csv"          # Name of input dataset file
+
+SCALE_DATASET     <- TRUE                 # Set to true to scale dataset before ML stage
+OUTLIER_CONF      <- 0.95                 # Confidence p-value for outlier detection
+
+TYPE_DISCREET     <- "DISCREET"           # field is discreet (numeric)
+TYPE_ORDINAL      <- "ORDINAL"            # field is continuous numeric
+TYPE_SYMBOLIC     <- "SYMBOLIC"           # field is a string
+TYPE_NUMERIC      <- "NUMERIC"            # field is initially a numeric
+TYPE_IGNORE       <- "IGNORE"             # field is not encoded
+
+DISCREET_BINS     <- 5                    # Number of empty bins to determine discreet
+
+SOM_EPOCHS        <- 2000                 # SOM training training epochs
+SOM_LEARN_RATE    <- c(0.05,0.01)         # SOM learning rate - see documentation
+SOM_GRIDSIZE      <- 10                   # SOM a 20x20 grid of neurons
+SOM_TYPE          <- "hexagonal"          # SOM neuron grid shape (also "rectangular")
+
+
+MYLIBRARIES<-c("outliers",
+               "corrplot",
+               "MASS",
+               "pROC",
+               "formattable",
+               "stats",
+               "caret",
+               "PerformanceAnalytics",
+               "dplyr",
+               "stats",
+               "anchors",
+               "kohonen")
+
+
+
+# ************************************************
+# main() :
+#
+# Keeps all objects as local to this function
+# ************************************************
+main<-function(){
+  # Read dataset
+  global_terrorism <- read.csv(DATASET_FILENAME)
+  
+  # Includes only incidents after 1997, where all incidents represent an act of terrorism 
+  after_1997 <- global_terrorism[global_terrorism$iyear >= 1997 & global_terrorism$doubtterr== 0,]
+  
+  post_feature_selection <- data.frame(
+    "Region" = after_1997$region,
+    "Attack_Type" = after_1997$attacktype1,
+    "Perpetrators_Number" = after_1997$nperps,
+    "Kill_Count" = after_1997$nkill,
+    "Wounded_Count" = after_1997$nwound
+  )
+  
+  install.packages("anchors")
+  library(anchors)
+  install.packages("dplyr")
+  library(dplyr)
+  
+  # Filter data for moments calculations
+  filtered_killed <- post_feature_selection %>% filter(!is.na(Kill_Count) & Kill_Count>=0 ) 
+  filtered_wounded_no_na_no_zero <- post_feature_selection %>% filter(!is.na(Wounded_Count) & Wounded_Count>0)
+  
+  
+  # Thresholds
+  killed_threshold <- round(mean(filtered_killed$Kill_Count)) 
+  wounded_threshold <- round(mean(filtered_wounded_no_na_no_zero$Wounded_Count))
+  perpetrator_mean <- computeRoundedMean(post_feature_selection, "Perpetrators_Number")
+  
+  # Transform Dataset
+  df <- replace.value(post_feature_selection, c("Kill_Count"), from=NA, to=as.double(killed_threshold))
+  df <- replace.value(df, c("Wounded_Count"), from=NA, to=as.double(wounded_threshold))
+  df <- subset(df, !is.na(Region))
+  df <- subset(df, (Attack_Type != 9))
+  
+  df <- df %>% mutate(Perpetrators_Number = case_when(is.na(Perpetrators_Number) | Perpetrators_Number < 0 ~ perpetrator_mean, TRUE ~ as.double(Perpetrators_Number)))
+  
+  
+  #countries<- loans %>% group_by(Country) %>% summarize(count=n())
+  # regions<- df %>% group_by(Region) %>% summarize(count=n())
+  # attack_type <- df %>% group_by(Attack_Type) %>% summarize(count=n())
+  # weapon_type <- df %>% group_by(Weapon_Type) %>% summarize(count=n())
+  
+  north_america <- subset(df, (Region==1))
+  central_america <- subset(df, (Region==2))
+  south_america <- subset(df, (Region==3))
+  east_asia <- subset(df, (Region==4))
+  south_east_asia <- subset(df, (Region==5))
+  south_asia <- subset(df, (Region==6))
+  central_asia <- subset(df, (Region==7))
+  western_europe <- subset(df, (Region==8))
+  eastern_europe <- subset(df, (Region==9))
+  middle_east <- subset(df, (Region==10))
+  sub_saharan_africa <- subset(df, (Region==11))
+  australia_oceania <- subset(df, (Region==12))
+  
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  # # # # # # # # # # # # # # # # # # # # #
+  
+  
+  som_df <- south_asia[ , -which(colnames(middle_east)=="Region")]
+  som_df_name <- "south_asia"
+  
+  som_df <- transformNumeric(som_df)
+  
+  data_train_matrix <- as.matrix(scale(som_df))
+  
+  dev.off()
+  
+  #Creates the SOM neuron map  dist.fcts="euclidean"
+  som_grid = kohonen::somgrid(SOM_GRIDSIZE, SOM_GRIDSIZE, SOM_TYPE)
+  
+  som_model <- kohonen::som(data_train_matrix,
+                            grid=som_grid,
+                            rlen=SOM_EPOCHS,
+                            alpha=SOM_LEARN_RATE,
+                            keep.data = TRUE)
+
+  
+  
+  # Progression of the learning process
+  plot(som_model, type="changes")
+  
+  # The number of instances into the cells are used to identify high-density areas
+  plot(som_model, type="count")
+  
+  # Neighbour distance plot. Called “U-Matrix” (unified distance matrix)
+  plot(som_model, type="dist.neighbours", main = "SOM neighbour distances")
+  
+  # Codebook vectors. This chart represents the vector of weights in a pie chart for each cell of the map.
+  plot(som_model, type="codes") 
+  
+  plot(som_model, type="mapping")
+  plot(som_model, type="quality") 
+  
+
+  
+  coolBlueHotRed <- function(n, alpha = 1) {
+    rainbow(n, end=4/6, alpha=alpha)[n:1]
+  }
+  
+
+  
+
+  while (!is.null(dev.list()))  dev.off();
+  
+  som_codes<-data.frame(som_model$codes)
+ for (i in 1:ncol(som_df)){
+  while (!is.null(dev.list()))  dev.off()
+  jpeg_name = paste("rplot_", som_df_name, "_", i, ".jpg", sep="")
+  jpeg(jpeg_name)
+  plot(som_model, 
+       type = "property",
+       property = som_codes[,i], 
+       main=names(data.frame(som_model$data))[i],
+       palette.name=coolBlueHotRed
+  )
+  dev.off()
+ }
+  
+ 
+  
+} 
+
+
+# clears the console area
+cat("\014")
+
+#library(pacman)
+#pacman::p_load(char=MYLIBRARIES,install=TRUE,character.only=TRUE)
+
+# load functions from the labs, all code written by Prof. Nick Ryman-Tubb
+source("SOM_lab_functions.R")
+source("preprocessingFunctions.R")
+
+
+set.seed(123)
+
+
+main()
+
+print("end")
+
+gc() # garbage collection to automatically release memory
+# clear plots and other graphics
+if(!is.null(dev.list())) dev.off()
+graphics.off()
+
+# clears the console area
+cat("\014")
+
+print("START Self Organising Maps")
+
+# specify libraries to be loaded by pacman
+myLibraries<-c(
+  "readr",
+  "ggplot2",
+  "dplyr",
+  "tidyverse",
+  "DT",
+  "plotly",
+  "highcharter",
+  "viridis",
+  "som",
+  "kohonen"
+)
+
+
+#library(pacman)
+#pacman::p_load(char=myLibraries,install=TRUE,character.only=TRUE)      
+
+set.seed(123)
+
+main()
+
+print("end of script")
+
